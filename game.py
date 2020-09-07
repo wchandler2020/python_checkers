d71855cd686264f4c3df28f4a2b58e0dc6ff5bef
Your Name <william.d.chandler1.com>
2020-09-07 08:06:48 -0400

    initial commit

---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+from checkers.board import Board
+
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+
+def game():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+        board.draw_squares(WIN)
+        pygame.display.update()
+
+    pygame.quit()
+game()
